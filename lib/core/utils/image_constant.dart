24a831bab9f35b4fc8350e96cb4d38c66f8ab173
Milad Khnefes <milad.khnefes.16@gmail.com>
2025-09-17 19:11:25 +0300

    Added some utils

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,23 @@
+class ImageConstant {
+  static final String _basePath = 'assets/images/';
+
+  static String imgPlaceholder = '${_basePath}placeholder.png';
+
+  static String imgEmojioneWaving = '${_basePath}img_emojione_waving.svg';
+  static String imgFlatcoloriconsgoogle = '${_basePath}img_flatcoloriconsgoogle.svg';
+  static String imgGroup710 = '${_basePath}img_group_710.svg';
+  static String imgLogoSymbol = '${_basePath}img_logo_symbol.svg';
+  static String imgPolygon6 = '${_basePath}img_polygon_6.svg';
+  static String imgSearch = '${_basePath}img_search.svg';
+  static String imgWorkiom = '${_basePath}img_workiom.svg';
+
+  static String imgConfiguration = '${_basePath}img_configuration.svg';
+  static String imgConfigurationShow = '${_basePath}img_configuration_show.svg';
+  static String imgFieldEmail = '${_basePath}img_field_email.svg';
+  static String imgFrame827237 = '${_basePath}img_frame_827237.svg';
+  static String imgFrame827298 = '${_basePath}img_frame_827298.svg';
+
+  static String imgArrowLeft = '${_basePath}img_arrow_left.svg';
+
+  static String imgImageNotFound = '${_basePath}image_not_found.png';
+}
